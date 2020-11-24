ef65bc6b40f0332319c27715d24a3ee58c1ef79c
cdROma <mrv24ru@yandex.ru>
2020-11-24 20:45:40 +0300

    fix: config env updated

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,14 +3,15 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: _app.env.SVELTE_APP_APIKEY,
-  authDomain: _app.env.SVELTE_APP_AUTHDOMAIN,
-  databaseURL: _app.env.SVELTE_APP_DB,
-  projectId: _app.env.SVELTE_APP_PID,
-  storageBucket: _app.env.SVELTE_APP_SB,
-  messagingSenderId: _app.env.SVELTE_APP_SID,
-  appId: _app.env.SVELTE_APP_APPID,
-  measurementId: _app.env.SVELTE_APP_MID,
+  apiKey: _app.env.SVELTE_APP_APIKEY || process.env.SVELTE_APP_APIKEY,
+  authDomain:
+    _app.env.SVELTE_APP_AUTHDOMAIN || process.env.SVELTE_APP_AUTHDOMAIN,
+  databaseURL: _app.env.SVELTE_APP_DB || process.env.SVELTE_APP_DB,
+  projectId: _app.env.SVELTE_APP_PID || process.env.SVELTE_APP_PID,
+  storageBucket: _app.env.SVELTE_APP_SB || process.env.SVELTE_APP_SB,
+  messagingSenderId: _app.env.SVELTE_APP_SID || process.env.SVELTE_APP_SID,
+  appId: _app.env.SVELTE_APP_APPID || process.env.SVELTE_APP_APPID,
+  measurementId: _app.env.SVELTE_APP_MID || process.env.SVELTE_APP_MID,
 };
 
 firebase.initializeApp(firebaseConfig);
