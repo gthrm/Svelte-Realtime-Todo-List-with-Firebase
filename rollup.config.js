90920fc621c7592ce46b4a980157e975420ba0f8
cdROma <mrv24ru@yandex.ru>
2020-11-24 21:17:13 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -55,14 +55,7 @@ export default {
       _app: JSON.stringify({
         env: {
           isProd: production,
-          SVELTE_APP_APIKEY: process.env.SVELTE_APP_APIKEY,
-          SVELTE_APP_AUTHDOMAIN: process.env.SVELTE_APP_APIKEY,
-          SVELTE_APP_DB: process.env.SVELTE_APP_APIKEY,
-          SVELTE_APP_PID: process.env.SVELTE_APP_APIKEY,
-          SVELTE_APP_SB: process.env.SVELTE_APP_APIKEY,
-          SVELTE_APP_SID: process.env.SVELTE_APP_APIKEY,
-          SVELTE_APP_APPID: process.env.SVELTE_APP_APIKEY,
-          SVELTE_APP_MID: process.env.SVELTE_APP_APIKEY,
+          ...process.env,
           ...config().parsed, // attached the .env config
         },
       }),
